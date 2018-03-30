88df36b504e9e0419091547b94b3563b85c7f95c
Wasin Thonkaew <haxpor@gmail.com>
2018-03-30 09:26:16 +0800

    Add ability to detect QRCode from URL, and normal text
    URL detection needs to be touched up again for complex URL.

--- qrcoderly/qrcoderly/ScanQRCodeViewController.swift
+++ qrcoderly/qrcoderly/ScanQRCodeViewController.swift
@@ -9,10 +9,11 @@
 import Cocoa
 import AVFoundation
 
-class ScanQRCodeViewController: NSViewController {
+class ScanQRCodeViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     @IBOutlet weak var customView: CustomView!
     @IBOutlet weak var helpButton: NSButton!
+    @IBOutlet weak var resultTextField: NSTextField!
     
     let helpPopover = NSPopover()
     
@@ -21,6 +22,11 @@ class ScanQRCodeViewController: NSViewController {
     var videoCompression: AVCaptureConnection?
     var stillImageOutput: AVCaptureStillImageOutput?
     var previewLayer: AVCaptureVideoPreviewLayer?
+    var overlayLayer: CAShapeLayer?
+    var qrcodeDetector: CIDetector?
+    
+    var previouslyMessageStringDetected: String?
+    var isOkToOpenNativeBrowserTabAgainIfMessageStringIsTheSame: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +41,7 @@ class ScanQRCodeViewController: NSViewController {
         // set up avfoundation to capture video and show preview on child view layer
         initCaptureSession()
         setupPreviewLayer()
+        setupOverlayLayer()
     }
     
     override func viewWillAppear() {
@@ -113,6 +120,26 @@ extension ScanQRCodeViewController {
                 if session.canAddInput(deviceInput) {
                     session.addInput(deviceInput)
                 }
+                
+                // create qrcode detector
+                let options = [CIDetectorAccuracy: CIDetectorAccuracyLow]
+                qrcodeDetector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: options)
+                
+                // make a video data output
+                let videoDataOutput = AVCaptureVideoDataOutput()
+                let rgbOutputSettings:[String:Any] = [kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: kCMPixelFormat_32BGRA)]
+                videoDataOutput.videoSettings = rgbOutputSettings
+                videoDataOutput.alwaysDiscardsLateVideoFrames = true
+                
+                // create a serial dispatch queue to process sample buffer
+                let queue = DispatchQueue(label: "videoDataBufferSerialQueue")
+                videoDataOutput.setSampleBufferDelegate(self, queue: queue)
+                if session.canAddOutput(videoDataOutput) {
+                    session.addOutput(videoDataOutput)
+                }
+                // get the output for doing face detection
+                videoDataOutput.connection(with: AVMediaType.video)!.isEnabled = true
+                
             } catch {
                 print("error creating deviceInput")
             }
@@ -143,6 +170,103 @@ extension ScanQRCodeViewController {
         if session.isRunning {
             session.stopRunning()
         }
+    }
+    
+    func setupOverlayLayer() {
+        if session != nil {
+            overlayLayer = CAShapeLayer()
+            overlayLayer!.lineWidth = 2
+            overlayLayer!.strokeColor = NSColor.yellow.cgColor
+            overlayLayer!.fillColor = NSColor.clear.cgColor
+            // note: for the paths we will update it real-time when it's started and we detecte rectangular shape in video stream
+            
+            view.layer!.addSublayer(overlayLayer!)
+        }
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // get the image
+        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+        let attachments:[String:Any] = CMCopyDictionaryOfAttachments(kCFAllocatorDefault, sampleBuffer, kCMAttachmentMode_ShouldPropagate) as! [String : Any]
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer!, options: attachments)
+        
+        detectQRCode(ciImage)
+    }
+    
+    func detectQRCode(_ ciImage: CIImage) {
+        guard let detector = qrcodeDetector else { return }
+        
+        // also apply orientation of image to detection
+        var options: [String:Any]
+        if ciImage.properties.keys.contains((kCGImagePropertyOrientation as String)) {
+            options = [CIDetectorImageOrientation: ciImage.properties[(kCGImagePropertyOrientation as String)] ?? 1]
+        }
+        else {
+            options = [CIDetectorImageOrientation: 1]
+        }
+        
+        // find features
+        let features = detector.features(in: ciImage, options: options)
+        
+        print("detect qrcode got features: \(features.count)")
+        
+        if features.count > 0 {
+            let qrcodeFeature = features.first as! CIQRCodeFeature
+            print("bounds for 1st feature: \(qrcodeFeature.bounds)")
+            // find the message of qrcode
+            print("message in qrcode: " + (qrcodeFeature.messageString ?? "empty"))
+            
+            // if we found message baked with qrcode then we continue our operation
+            if let messageString = qrcodeFeature.messageString {
+                // if deteced message string is not the same as previously detected, thus we go ahead
+                if previouslyMessageStringDetected == nil ||
+                    (previouslyMessageStringDetected != nil && messageString != previouslyMessageStringDetected) ||
+                    (previouslyMessageStringDetected != nil && messageString == previouslyMessageStringDetected && isOkToOpenNativeBrowserTabAgainIfMessageStringIsTheSame) {
+                    // open URL via native browser
+                    if _validateURL(messageString) {
+                        // update result textfield
+                        DispatchQueue.main.async {
+                            self.resultTextField.stringValue = messageString
+                        }
+                        
+                        // set flag that we've used the chance to open browser tab this time
+                        isOkToOpenNativeBrowserTabAgainIfMessageStringIsTheSame = false
+                        
+                        // open a new browser tab
+                        NSWorkspace.shared.open(URL(string: messageString)!)
+                        
+                        // for dispatched queue execution code
+                        let tempURL = messageString
+                        
+                        // schedule to make it ok to open browser tab again after cooldown time
+                        DispatchQueue.global(qos: .default).asyncAfter(deadline: .now() + Settings.COOLDOWN_UNTIL_OPEN_NEW_BROWSER_TAB) {
+                            // check only if such message is the same as previuosly, so we gonna allow openning a new tab again
+                            // this is to prevent mess state as user can immediately scan a new qrcode (thus new URL)
+                            if self.previouslyMessageStringDetected != nil && tempURL == self.previouslyMessageStringDetected {
+                                self.isOkToOpenNativeBrowserTabAgainIfMessageStringIsTheSame = true
+                                print("set ok to open a new tab again")
+                            }
+                        }
+                    }
+                        // just show result on UI
+                    else {
+                        DispatchQueue.main.async {
+                            self.resultTextField.stringValue = messageString
+                        }
+                    }
+                }
+                
+                // update detected message string
+                previouslyMessageStringDetected = messageString
+            }
+        }
+    }
+    
+    // solution from https://stackoverflow.com/a/37065820/571227
+    private func _validateURL(_ urlString: String) -> Bool {
+        let urlRegEx = "((?:http|https)://)?(?:www\\.)?[\\w\\d\\-_]+\\.\\w{2,3}(\\.\\w{2})?(/(?<=/)(?:[\\w\\d\\-./_]+)?)?"
+        let urlTest = NSPredicate(format:"SELF MATCHES %@", urlRegEx)
+        return urlTest.evaluate(with: urlString)
     }
 }
 
---
+++ qrcoderly/qrcoderly/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  qrcoderly
+//
+//  Created by Wasin Thonkaew on 3/30/18.
+//  Copyright © 2018 Wasin Thonkaew. All rights reserved.
+//
+
+import Foundation
+
+final class Settings {
+    public static let COOLDOWN_UNTIL_OPEN_NEW_BROWSER_TAB: TimeInterval = 3  // in seconds
+}
